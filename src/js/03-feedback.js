8f182becbf16d3ac3fbae5a380edbb98868b4e65
Landar-S <landarsergij@gmail.com>
2022-10-03 15:58:36 +0300

    task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+const form = document.querySelector('.feedback-form');
+const throttle = require('lodash.throttle');
+const FEEDBACK_KEY = 'feedback-form-state';
 
+form.addEventListener('input', throttle(saveFeedback, 500));
+form.addEventListener('submit', onSubmit);
+
+loadFeedback();
+
+function saveFeedback() {
+  const feedback = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+
+  localStorage.setItem(FEEDBACK_KEY, JSON.stringify(feedback));
+}
+
+function loadFeedback() {
+  if (localStorage.getItem(FEEDBACK_KEY)) {
+    const parsedFeedback = JSON.parse(localStorage.getItem(FEEDBACK_KEY));
+    form.elements.email.value = parsedFeedback.email;
+    form.elements.message.value = parsedFeedback.message;
+  }
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  if (!e.currentTarget.email.value || !e.currentTarget.message.value) {
+    alert('Please fill in all the fields!');
+  } else {
+    if (JSON.parse(localStorage.getItem(FEEDBACK_KEY)) !== null) {
+      console.log(JSON.parse(localStorage.getItem(FEEDBACK_KEY)));
+    }
+
+    form.reset();
+    localStorage.clear();
+  }
+}
